df49ef84b23f51f714dd87f2a1b8ecefe96dab91
ajarwee <ajarwee@byui.edu>
2023-10-18 19:22:58 -0600

    changes made

---
+++ myName.test.js
@@ -0,0 +1,20 @@
+// myName.test.js
+
+const { expect } = require('chai');
+const { getMyName } = require('./myName'); // Import the getMyName function
+
+// Describe the test suite
+describe('Name Function', () => {
+
+  // Describe the specific function you're testing
+  describe('getMyName', () => {
+
+    // Write your test case
+    it('should return your name', () => {
+      const result = getMyName(); // Call the function
+      expect(result).to.equal('Your Name'); // Check if the result matches your name
+    });
+
+  });
+
+});
