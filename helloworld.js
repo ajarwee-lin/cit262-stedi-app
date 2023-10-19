347abddf2b172097b72022007375802e3a2c9876
ajarwee <ajarwee@byui.edu>
2023-10-18 18:29:54 -0600

    changes

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+// Define a function to return your name
+function getMyName() {
+    return "John Doe"; // Replace with your name
+  }
+  
+  // Call the function and print "Hello" along with the name to the console
+  console.log("Hello " + getMyName());
+  
