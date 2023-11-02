08a4cb7ee9bd085134f606506f3f008b9552a505
ajarwee <ajarwee@byui.edu>
2023-11-01 20:29:23 -0600

    change

---
+++ login.test.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch');
+
+it("should get a login token", asyn({userName, password})=>{
+
+
+    let loginToken="";//this is not what it will be eventually
+
+expect.(loginToken.length).toBe(36);//check for a valid token
+}
+);
